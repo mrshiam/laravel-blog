850e40b9626610d0138b9a06f341a20e51c3c62e
mrshiam <mrshiam01@gmail.com>
2022-01-10 18:02:08 +0600

    user created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 Route::get('dashboard',function (){
     return view('admin.dashboard');
 });
+Route::resource('user',UserController::Class);
