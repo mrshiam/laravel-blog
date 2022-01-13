ac80d62f283be6ce90a36fe0e8e3d8ececb30d17
mrshiam <mrshiam01@gmail.com>
2022-01-13 17:23:06 +0600

    created helper to update pagination

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+ function managePaginationSerial($obj)
+{
+    $serial = 1;
+    if($obj->currentPage() > 1)
+    {
+        $serial = $obj->perPage()*($obj->currentPage()-1)+1;
+    }
+    return $serial;
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,8 @@ class UserController extends Controller
     {
         $data['title'] = 'User List';
         $data['users'] = User::paginate(1);
-        $data['serial'] = ($data['users']->currentPage() != 1)?($data['users']->perPage()*($data['users']->currentPage()-1))+1:1;
+        $data['serial'] = managePaginationSerial($data['users']);
+        //$data['serial'] = ($data['users']->currentPage() != 1)?($data['users']->perPage()*($data['users']->currentPage()-1))+1:1;
         return view('admin.user.index',$data);
     }
 
